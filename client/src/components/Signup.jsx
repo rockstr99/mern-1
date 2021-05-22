f3baf10c7069d62674d8ac74f0e7b312edc4f752
Arpit Khandelwal <57443699+rockstr99@users.noreply.github.com>
2021-05-22 19:30:49 +0530

    created navbar for the frontend

---
+++ client/src/components/Signup.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Signup = () => {
+  return (
+    <div>
+      <p>Welcome</p>
+      <h1>We are MERN developer</h1>
+    </div>
+  );
+};
+
+export default Signup;
